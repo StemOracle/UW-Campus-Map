ea4cae33b390f3cf9a79d292a67101c794ad6b3e
Charlez Hamilton-Eppler <charlez@jlee.cs.washington.edu>
2023-08-10 12:57:49 -0700

    First few parts complete.

--- hw-lines/src/EdgeList.tsx
+++ hw-lines/src/EdgeList.tsx
@@ -10,33 +10,81 @@
  */
 
 import React, {Component} from 'react';
+import MapLine from './MapLine';
+import Edge from './Edge';
 
 interface EdgeListProps {
-    onChange(edges: any): void;  // called when a new edge list is ready
-                                 // TODO: once you decide how you want to communicate the edges to the App, you should
-                                 // change the type of edges so it isn't `any`
+  onChange(lines: Edge[]): void;  // called when a new edge list is ready
+}
+
+interface EdgeListState {
+  text: string; // Text stored in the text box
 }
 
 /**
  * A text field that allows the user to enter the list of edges.
  * Also contains the buttons that the user will use to interact with the app.
  */
-class EdgeList extends Component<EdgeListProps> {
-    render() {
-        return (
-            <div id="edge-list">
-                Edges <br/>
-                <textarea
-                    rows={5}
-                    cols={30}
-                    onChange={() => {console.log('textarea onChange was called');}}
-                    value={"I'm stuck..."}
-                /> <br/>
-                <button onClick={() => {console.log('Draw onClick was called');}}>Draw</button>
-                <button onClick={() => {console.log('Clear onClick was called');}}>Clear</button>
-            </div>
-        );
+class EdgeList extends Component<EdgeListProps, EdgeListState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  parse(text: string): Edge[] {
+    const lines: string[] = text.split("\n");
+    let edges: Edge[] = [];
+
+    let i = 0;
+    // Inv: Up to current, all lines are parsed into an Edge object and added.
+    // Alternatievly, if badly formatted, they are rejected.
+    while(i < lines.length) {
+      const parsed: string = lines[i];
+      const tokens: string[] = parsed.split(" ");
+      if(tokens.length !== 5) {
+        console.log("Line: " + parsed + " was improperly formatted. Too many tokens.");
+        continue;
+      }
+      // To avoid errors but will probably change to something else.
+      const x1: any = +lines[0]; const y1: any = +lines[1];
+      const x2: any = +lines[2]; const y2: any = +lines[3];
+      // To be more forgiving with casing.
+      const col: any = lines[4].toLowerCase();
+      if(typeof x1 !== 'number' || typeof y1 !== 'number' || typeof x2 !== 'number'
+                              || typeof y2 !== 'number' || typeof col !== 'string') {
+        console.log("Line: " + parsed + " was improperly formatted. Incorrect token type.");
+        continue;
+      } else if((x1 < 0) || (x1 > 4000) || (y1 < 0) || (y1 > 4000) || (x2 < 0) || (x2 > 4000)
+                                        || (y2 < 0) || (y2 > 4000)) {
+        console.log("Line: " + parsed + " has a start or end coordinate out of bounds.");
+        continue;
+      }
+      edges.push(new Edge(x1, y1, x2, y2, col));
     }
+    return edges;
+  }
+
+  render() {
+    return ( 
+      <div id="edge-list">
+        Edges <br/>
+          <MapLine color="red" x1={0} y1={0} x2={0} y2={0} />
+          <textarea
+            rows={5}
+            cols={30}
+            // Can try ChangeEventHandler<HTMLTextAreaElement>
+            // Changes the text area in the text box.
+            onChange={(event: any) => {this.setState({text: event.target.value});}}
+            value={this.state.text}
+          /> <br/>
+        <button onClick={() => {this.props.onChange(this.parse(this.state.text));}}>Draw</button>
+        <button onClick={() => {console.log('Clear onClick was called');}}>Clear</button>
+      </div>
+      );
+  }
 }
 
 export default EdgeList;
